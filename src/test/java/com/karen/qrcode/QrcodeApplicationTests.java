eb7b0419548245135d6562ac7ebe9f15e51490d5
陈权伟 <chenquanwei@coocaa.com>
2020-08-18 15:47:27 +0800

    Initial commit

---
+++ src/test/java/com/karen/qrcode/QrcodeApplicationTests.java
@@ -0,0 +1,14 @@
+package com.karen.qrcode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
